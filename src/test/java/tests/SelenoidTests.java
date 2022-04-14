689a01380f72a325defa83fcd049dd9f057c91b8
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-04-14 13:06:51 +0300

    add check Total tests

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -1,8 +1,31 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.is;
+
 public class SelenoidTests {
     //make request to https://selenoid.autotests.cloud/status
     //total is 20
+
+    @Test
+    void checkTotal() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalwithoutGiven() {
+
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
 
 
 }
